47723a8c2c055fa9b2ba82acf08c4edc8a41862a
Developer <glara.styleweb@gmail.com>
2013-03-10 02:07:45 -0600

    Primer colocacion de estilos

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <html>
 <title>GUI 4</title>
 <head>
-
+<link rel="stylesheet" type="text/css" href="css/estilos.css" >
 </head>
 <body>
-
+<div id="contenedorPrincipal">
+	<div id="barraSuperior">
+		<div id="tituloPrincipal">Intranet IQelectronics</div>
+		<div id="tituloDer">Ayuda | LogIn</div>
+	</div>
+	<div id="contenidoPrincipal">
+		<div id="contenedorLogIn"></div>
+		<div id="contenedorLogInDer"></div>
+		<div id="barraPie">Hola</div>
+	</div>
+</div>
 </body>
 </html>
