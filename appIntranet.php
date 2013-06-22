8c745394ee5865d71b5bfa0c1b71b0d34ebc54d1
Developer <glara.styleweb@gmail.com>
2013-06-22 18:38:18 -0500

    Modificacion de estilos

--- appIntranet.php
+++ appIntranet.php
@@ -77,7 +77,7 @@
 	    </div>
 	    <div id="infoIzq" style="float: left;border: 0px solid #CCC;margin: 5px;width: 78%;overflow: auto;">
 		
-		<div style="border-top: 1px solid #666;border-left: 1px solid #666;height: auto;width: 98%;margin: 20px 5px 45px 5px;">
+		<div style="border-top: 1px solid #CCC;border-left: 1px solid #CCC;height: auto;width: 98%;margin: 20px 5px 45px 5px;">
 		    <div style="border: 1px solid #CCC;background: #f0f0f0;width: 300px;height: 20px;padding: 5px;margin: -15px 5px 15px 5px;font-size: 14px;">Titulo Tipo App</div>
 		    <div class="estiloAppInfo">
 			<div style="float: left;width: 45px;height: 45px;border: 1px solid #FF0000;margin: 11px 5px 5px 5px;">&nbsp;</div>
